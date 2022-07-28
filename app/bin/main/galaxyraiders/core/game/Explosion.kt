8688256a0ce59fdd36f398c86cc7528dc974e811
Daniel Silva Lopes da Costa <dslcosta2016@usp.br>
2022-07-28 13:27:25 -0300

    Explosion Feature
    Co-authored-by: Daniel Silva Lopes da Costa  <dslcosta2016@usp.br>
    Co-authored-by: Marília Dicezare <mariliatd@gmail.com>

---
+++ app/bin/main/galaxyraiders/core/game/Explosion.kt
@@ -0,0 +1,23 @@
+package galaxyraiders.core.game
+
+import galaxyraiders.core.physics.Point2D
+import galaxyraiders.core.physics.Vector2D
+
+class Explosion(
+  initialPosition: Point2D,
+  initialVelocity: Vector2D,
+  radius: Double,
+  mass: Double,
+  time: Int
+) :
+  SpaceObject("Explosion", '#', initialPosition, initialVelocity, radius, mass) {
+  var time: Int = time
+
+  fun updateTime() {
+    time = time - 1
+  }
+
+  fun isTrigged(): Boolean {
+    return (time > 0)
+  }
+}
